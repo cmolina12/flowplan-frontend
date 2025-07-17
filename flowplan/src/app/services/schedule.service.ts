4dff90244b8d3e2c35a482a7ee4f349fcf9ccc08
cmolina12 <c.molinap@uniandes.edu.co>
2025-07-17 00:58:47 -0500

    schedule service

---
+++ flowplan/src/app/services/schedule.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SectionModel } from '../models/section-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+
+  private apiUrl = 'http://localhost:8080/api/schedule';
+
+
+constructor(private http: HttpClient) { }
+
+  getSchedules(sectionsPerCourse: SectionModel[][]): Observable<SectionModel[][]>{
+
+    return this.http.post<SectionModel[][]>(this.apiUrl, sectionsPerCourse);
+
+  }
+
+}
